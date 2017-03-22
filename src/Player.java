6c77a26f7bcecdc169f40797627ff27932edbeee
Niall <niall.grant1992@gmail.com>
2017-03-22 09:04:26 +0000

    Added the Player class. Currently empty

---
+++ src/Player.java
@@ -0,0 +1,21 @@
+/**
+ * This class will be used to hold and process Player information
+ * It will hold the player's Name, Carrots, Lettuce and Token
+ * 
+ * It will also have accessors and mutators for all 4 variables
+ * which will be accessible to other classes
+ * 
+ * @author Kevin Fan
+ * @author Niall Grant
+ * @author Bernadette Murphy
+ * @author Keelan Murphy
+ * @version 2017.03.21
+ *
+ */
+public class Player 
+{
+	public Player()
+	{
+		
+	}
+}
