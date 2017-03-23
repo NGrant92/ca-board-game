2611a5b12a7ac0631afb70e4401dc70f233b32d2
Keelan Murphy <keelanmurphy90@gmail.com>
2017-03-23 07:31:44 +0000

    Dealt with how a card is drawn from the deck of hare cards and shuffling the deck. Also added the Card class

---
+++ src/Card.java
@@ -0,0 +1,20 @@
+/**
+ * This class models a card from the Hare deck.
+ *
+ * @author Kevin Fan
+ * @author Niall Grant
+ * @author Bernadette Murphy
+ * @author Keelan Murphy
+ * @version 2017.03.21
+ *
+ */
+public class Card {
+  
+  public String title;
+  public String instruction;
+  
+  public Card(String title) {
+    this.title = title;
+  }
+
+}
