7e441b83bedd0374feec5a5084a30ec5d12e30e9
Keelan Murphy <keelanmurphy90@gmail.com>
2017-04-02 19:07:55 +0100

    Added classes which will hamdle the board, alot of extremely messy code in order to be able to discuss how we can handle certain events

---
+++ src/CarrotSquare.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by keela on 01/04/2017.
+ */
+public class CarrotSquare {
+}
---
+++ src/Square.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by keela on 29/03/2017.
+ */
+public class Square {
+}
