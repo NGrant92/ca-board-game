8920462fe1e13027d071f6c8201c048404bfad5b
Niall <niall.grant1992@gmail.com>
2017-04-06 15:36:31 +0100

    checkPosition converts the tile name into an integer and compares it against player's position

--- src/models/PostionSquare.java
+++ src/models/PostionSquare.java
@@ -6,7 +6,11 @@ import java.util.ArrayList;
 
 /**
  * Created by Niall on 03/04/2017.
- * @Author Niall G
+ * @author Kevin Fan
+ * @author Niall Grant
+ * @author Bernadette Murphy
+ * @author Keelan Murphy
+ * @version 2017.04.03
  *
  * A position tile has a certain number(between 1 - 6) associated with it. If a player lands on that tile
  * and starts their next turn in the same position in the race as the specific number then they are rewarded
@@ -17,20 +21,44 @@ import java.util.ArrayList;
 public class PostionSquare extends Square {
 
     int recievedCarrots = 0;
-    int playerIndex = 0;
-    int tilePosition = 0;
 
     public PostionSquare(String name, int position) {
         super(name, position);
     }
 
-    //When they start their turn on a positionTile this method will check if
-    //they will recieve the carrots or not
+    //When they start their turn on a positionTile this method will check if they will recieve the carrots or not
+    //checkPoisiton is called from the Gamecontroller who inputs Player arrayList, currentPlayer & TilesArray Index
     public int checkPosition(ArrayList<Player> players, Player currentPlayer){
-            if(GameController.getPlayerPositionInRace() == tilePosition) {
-                recievedCarrots = tilePosition * 10;
-            }
+        //Square name is converted into an integer
+        int tileNum = Integer.parseInt(name);
+
+        //Testing if the player's position in race matches the required position
+        //returns the appropriate carrots
+        if(tileNum == getPlayerPositionInRace(currentPlayer)) {
+            recievedCarrots = tileNum * 10;
+        }
         return recievedCarrots;
+    }
+
+    /**
+     * Method to return the player position in race. Possibly can be used in number square or lettuce square
+     * calculation.
+     */
+    public int getPlayerPositionInRace(Player player) {
+
+        // local variable to store the players current position - not really need
+        int currentPlayerPositionOnBoard = player.getPosition();
+        // Local temporary store for playerPositionInRace
+        int playerPositionInRace = 1;
+
+        // Check each player position in players array, and increment playerPosition by 1 if the player position
+        // is less than currentPlayerPositionInRace
+        for (int i = 0; i < players.size(); i++) {
+            if (currentPlayerPositionOnBoard < players.get(i).getPosition()) {
+                playerPositionInRace++;
+            }
+        }
+        return playerPositionInRace;
     }
 
     //This will print to display how much carrots they have recieved, if any.
