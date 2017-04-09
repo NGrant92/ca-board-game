e050fc1fe8779e8464417fb3143cb1b355f03627
Niall <niall.grant1992@gmail.com>
2017-04-09 16:14:15 +0100

    Added lastTurnFree(), started shuffleCards() and changed setHalfCarrots() formula so it's less verbose

--- src/models/HareSquare.java
+++ src/models/HareSquare.java
@@ -1,5 +1,9 @@
 package models;
 
+import controllers.GameController;
+
+import java.util.ArrayList;
+
 /**
  * Created by kevin on 06/04/2017.
  * A class used to store the methods that represent the cards from the hare deck
@@ -41,7 +45,34 @@ public class HareSquare extends Square {
         }
     }
 
+    //=================
+    //LAST TURN COSTS 0
+    //=================
+    //A method to return the players previously expended amount of carrots
+    //To do this the player's previous position is deducted from their current position
+    //That result is put into a formula that calculates the required carrots for moving that distance
+    // which will add the amount to the player's pendingBalance
+    public void lastTurnFree(Player player){
 
+        //this subtracts player's previous position from their current position and feeds it into a variable name
+        int distance = (player.getPosition() - player.getPreviousPosition());
+
+        //int distance is used to calculate the amount of carrots the players previous move cost them
+        //and sends it into their pending balance
+        player.setPendingBalance(distance*(distance + 1)/2);
+    }
+
+    //=================
+    //SHUFFLE CARD DECK
+    //=================
+    //The player must shuffle the card deck and recieve 1 carrot from each player
+    //TODO find a way to call HareDeck.shuffle()
+    public void shuffleCards(ArrayList<Player> players, Player currentPlayer){
+
+        HareDeck.shuffle();
+
+        currentPlayer.setPendingBalance(players.size() - 1);
+    }
 
     //==================
     //BACK TO 65 CARROTS
@@ -56,7 +87,8 @@ public class HareSquare extends Square {
     //=================
     //HALF YOUR CARROTS
     //=================
-    //This will be the method called by the Execute Rule class. Which will pass in the required player class
+    //This method is for halving the players stash of carrots. setHalfCarrots() returns the required int
+    //and halfCarrots() inputs it into the player's setPendingBalance()
     public void halfCarrots(Player player){
 
         //this method sets the pending balance to the amount of carrots
@@ -66,22 +98,11 @@ public class HareSquare extends Square {
         player.setPendingBalance(setHalfCarrots(player.getNoOfCarrots()));
     }
 
-    public int setHalfCarrots(int carrots){
+    public int setHalfCarrots(int carrots) {
 
-        //simple arguement to have the inputted carrots
-        int halfCarrots = carrots / 2;
-
-        //Checks if when the player's carrots are halved if there is a remainder
-        //if there is a remainder it will round up the answer
-        if(halfCarrots % 2 != 0) {
-            //the below method adds 1 to halfCarrots() because it is rounded down if halfCarrots() = x.5
-            //the rules specify if halving an odd number you should round up
-            return (halfCarrots + 1);
-        }
-        //returns halfCarrots() without any changes
-        else{
-            return halfCarrots;
-        }
+        //a method that divides the player's carrots in half and rounds up the answer if answer results in X.5
+        //eg: 65 / 2 = 32.5, this will make it 33
+        return ((int) Math.ceil(carrots / 2.0));
     }
 
 }
