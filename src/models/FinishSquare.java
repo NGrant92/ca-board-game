a6b0ddf00de94d86e5e120955f869c6bfc46217d
bernie <bernie>
2017-04-12 11:20:42 +0100

    finish square edited

--- src/models/FinishSquare.java
+++ src/models/FinishSquare.java
@@ -1,5 +1,5 @@
 /*This class is a subclass of square and it allows the player to land on the finish square
- * 
+ * in my CanmoveHere method I have overwrote the rules of this method 
  */
 package models;
 
@@ -9,7 +9,7 @@ public class FinishSquare extends Square {
 
 
 	public FinishSquare(String name, int position) {
-		super(name, position);
+		super(name, position canMoveHere );
 		// TODO Auto-generated constructor stub
 	}
 
@@ -18,16 +18,32 @@ public class FinishSquare extends Square {
 	 * It will pass the ArrayList and who ever is first in the Player ArrayList will be first and so on 
 	 */
 	public void printStandings()
-	{   for (int i=0; i<players.size();i++){}}
+	{   for (int i=0; i<players.size();i++){}
 	
 /*
  * For a player to  be able to finish the game they need to have gotten rid off all 
  * their lettuce cards and  They can only have a certain amount of carrots depending on what place they are in the game.
  * 
  */
+	public boolean canMoveHere(){
+		if (canMove==true){
+	}
 	
-		
-	//public void setNoOfLettuce
+	
+			 
+			if(players.getNoOfLettuce() == 0 && player.getNoOfCarrots() <= maxCarrots){   
+			  canMoveHere(true, currentPlayer);
+			}
+			else{
+				canMoveHere(false, currentPlayer);
+			}
+			public boolean canMoveHere(boolean canMove, Player currentPlayer)
+				if(canMove == true){
+					players.add(currentPlayer);
+				}
+	
+
+	//dependant on their place in the game they are only allowed have a certain number of carrots to finish 
 
 
 
