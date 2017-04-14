abe5d9fa7f0af69e076e41a0f61551c6c116d447
KevFan <kevin_fan@hotmail.co.uk>
2017-04-14 12:40:39 +0100

    Commented out unused methods

--- src/models/CarrotSquare.java
+++ src/models/CarrotSquare.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.ArrayList;
+
 /**
  * Created by keela on 01/04/2017.
  * Edited by Kevin on 06/04/2017.
@@ -21,6 +23,7 @@ public class CarrotSquare extends Square {
         super(name, position);
     }
 
+    // Todo: likely won't need this method - remove
 //    /**
 //     * Set the pending balance for the player to 10. Game controller would will allow the player to accept the 10
 //     * carrots or remove the 10 carrots to their balance.
@@ -35,40 +38,44 @@ public class CarrotSquare extends Square {
      * Carrot squares allows the player to stay indefinitely in exchange for not moving and adding/removing 10 carrots
      * @return Boolean value on whether the player can stay on the square - always return true for a carrot square
      */
+    @Override
     public boolean canStay() {
         return true;
     }
 
-    /** Check to see is player stayed on the carrot square for the turn
-     *
-     * @param player Player object passed in to get player information
-     */
-    public boolean check(Player player) {
-        if (player.getPreviousPosition() == player.getPosition()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // Todo: likely won't need this method - remove
+//    /** Check to see is player stayed on the carrot square for the turn
+//     *
+//     * @param player Player object passed in to get player information
+//     */
+//    public boolean check(Player player) {
+//        if (player.getPreviousPosition() == player.getPosition()) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
 
     /**
      * Apply rule method that is run each time the before the player moves to another tile, choose to stay or move backwards
      * in the game controller
      */
 
-    public String applyRule() {
-        String str = "";
-        if(check(players.get(0))) {
-            str = "Would you like to stay on carrot square? (Y/N)";
-        }
-        return str;
+    @Override
+    public String applyRule(ArrayList<Player> allPlayers) {
+//        String str = "";
+//        if(check(players.get(0))) {
+//            str = "If you choose to stay on carrot square, you can choose to gain or remove 10 carrots!";
+//        }
+        return "If you choose to stay on carrot square, you can choose to gain or remove 10 carrots!";
     }
 
-    /**
-     * Returns a string of player's carrot balance
-     * @return String of player's carrot balance
-     */
-    public String toString() {
-        return "You now have " + players.get(0).getNoOfCarrots() + "carrots";
-    }
+    // Todo: likely won't need this method - remove
+//    /**
+//     * Returns a string of player's carrot balance
+//     * @return String of player's carrot balance
+//     */
+//    public String toString() {
+//        return "You now have " + players.get(0).getNoOfCarrots() + "carrots";
+//    }
 }
